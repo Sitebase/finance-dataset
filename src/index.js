f359d22760d71013806e5891bb7e227646403093
Wim Mostmans <wim@sitebase.be>
2022-02-19 22:21:31 +0100

    initial version

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const coins = require('coinlist');
+
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
+});
+
+/**
+ * Many more examples available at:
+ *   https://developers.cloudflare.com/workers/examples
+ * @param {Request} request
+ * @returns {Promise<Response>}
+ */
+async function handleRequest(request) {
+  const { pathname } = new URL(request.url);
+
+  if (pathname.startsWith("/api")) {
+    return new Response(JSON.stringify({ pathname }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  if (pathname.startsWith("/crypto/coins")) {
+    return new Response(JSON.stringify(coins), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  if (pathname.startsWith("/status")) {
+    const httpStatusCode = Number(pathname.split("/")[2]);
+
+    return Number.isInteger(httpStatusCode)
+      ? fetch("https://http.cat/" + httpStatusCode)
+      : new Response("That's not a valid HTTP status code.");
+  }
+
+  return fetch("https://welcome.developers.workers.dev");
+}
