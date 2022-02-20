fa8d173091b40fe62f0e717b119181577c32b44a
Wim Mostmans <wim@sitebase.be>
2022-02-20 13:46:25 +0100

    add bitcoin as currency

--- scripts/currencies.js
+++ scripts/currencies.js
@@ -18,12 +18,15 @@ async function safeQuote(quote) {
 const eur = await safeQuote('EURUSD=X');
 const silver = await safeQuote('SI=F');
 const gold = await safeQuote('GC=F');
+const btc = await safeQuote('BTC-USD');
 
 const data = {
     eur: eur.regularMarketPrice,
     silver: silver.regularMarketPrice,
     gold: gold.regularMarketPrice,
+    btc: btc.regularMarketPrice,
     created: new Date().toISOString()
 }
 
+console.log('result', data);
 await fs.writeFile('artifacts/currencies.json', JSON.stringify(data, null, 2), { flag: 'w+' });
