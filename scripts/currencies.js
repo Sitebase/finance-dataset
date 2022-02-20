8de17ad450c8e325f13f8766cebae124b551384a
Wim Mostmans <wim@sitebase.be>
2022-02-20 13:50:06 +0100

    add some extra currencies

--- scripts/currencies.js
+++ scripts/currencies.js
@@ -19,11 +19,18 @@ const eur = await safeQuote('EURUSD=X');
 const silver = await safeQuote('SI=F');
 const gold = await safeQuote('GC=F');
 const btc = await safeQuote('BTC-USD');
+const jpy = await safeQuote('JPYUSD=X');
+const gbp = await safeQuote('GBPUSD=X');
+const chf = await safeQuote('CHFUSD=X');
 
 const data = {
     eur: eur.regularMarketPrice,
     silver: silver.regularMarketPrice,
     gold: gold.regularMarketPrice,
+    btc: btc.regularMarketPrice,
+    jpy: jpy.regularMarketPrice,
+    gbp: gbp.regularMarketPrice,
+    chf: chf.regularMarketPrice,
     btc: btc.regularMarketPrice,
     created: new Date().toISOString()
 }
